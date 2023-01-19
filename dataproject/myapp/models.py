562b96649e396611765afe44bee3c2beddb35861
ranjanajothimanikandan <11g03.ranjana.j@gmail.com>
2023-01-19 06:17:29 +0000

    Success

--- dataproject/myapp/models.py
+++ dataproject/myapp/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.contrib import admin
-class Employee (models.Model):
+class Employee(models.Model):
     eid=models.CharField(max_length=20,help_text="Employee ID")
     name=models.CharField(max_length=100)
     salary=models.IntegerField()
     age=models.IntegerField()
     email=models.EmailField()
-    class EmployeeAdmin(admin.ModelAdmin):
-        list_display=('eid','name','salary','age','email')
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display=('eid','name','salary','age','email')
